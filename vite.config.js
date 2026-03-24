d453eeb72aba0b744311bbea0570f7aac3ca8b37
Alessandro Saldanha <alessandrosaldanha.as@gmail.com>
2026-03-24 10:36:53 -0300

    "Feat: Vite Config"

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/React_Leo/",
+  // Mude de "/React_Leo/" para "/" ou "./"
+  base: "/",
   plugins: [react()],
   server: {
     port: 3000,
